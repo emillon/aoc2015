3b30dd284c503205d6deeaac7c8307c71c9bdde5
Etienne Millon <me@emillon.org>
2024-05-31 16:10:14 +0200

    day 12

---
+++ day12/day12.ml
@@ -0,0 +1,22 @@
+open Base
+open Lib
+
+let has_red = List.exists ~f:(function _, `String "red" -> true | _ -> false)
+
+let rec json_sum ~ignore_red = function
+  | `Bool _ -> 0
+  | `Assoc kvs ->
+      if ignore_red && has_red kvs then 0
+      else List.map kvs ~f:(fun (_, v) -> json_sum v ~ignore_red) |> sum
+  | `Int n -> n
+  | `List l -> List.map l ~f:(json_sum ~ignore_red) |> sum
+  | `Null -> 0
+  | `String _ -> 0
+  | `Float _ -> assert false
+  | `Intlit _ -> assert false
+  | `Tuple _ -> assert false
+  | `Variant _ -> assert false
+
+let f1 s = Yojson.Safe.from_string s |> json_sum ~ignore_red:false
+let f2 s = Yojson.Safe.from_string s |> json_sum ~ignore_red:true
+let run () = Lib.run ~name:"day12" ~f1 ~f2 Day12_input.data
