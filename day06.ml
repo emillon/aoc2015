21897ac646fad53b47da9bd9862850b972f18115
Etienne Millon <me@emillon.org>
2024-06-04 15:46:14 +0200

    opt

--- day06.ml
+++ day06.ml
@@ -1,15 +1,13 @@
 open Parsing_util
 
 type op = Turn_on | Turn_off | Toggle [@@deriving sexp]
-type instruction = op * int * int [@@deriving sexp]
-
-let key x y = (x * 1000) + y
+type instruction = op * Pos.t * Pos.t [@@deriving sexp]
 
 let parse =
   let position =
     let open Angstrom in
     let+ x = number <* string "," and+ y = number in
-    key x y
+    (x, y)
   in
   let op =
     let open Angstrom in
@@ -28,49 +26,53 @@ let parse =
 let%expect_test "parse" =
   let test s = parse s |> [%sexp_of: instruction] |> print_s in
   test "turn on 0,0 through 999,999";
-  [%expect {| (Turn_on 0 999999) |}];
+  [%expect {| (Turn_on (0 0) (999 999)) |}];
   test "toggle 0,0 through 999,0";
-  [%expect {| (Toggle 0 999000) |}];
+  [%expect {| (Toggle (0 0) (999 0)) |}];
   test "turn off 499,499 through 500,500";
-  [%expect {| (Turn_off 499499 500500) |}]
+  [%expect {| (Turn_off (499 499) (500 500)) |}]
 
-let quorem a = (a / 1000, Int.rem a 1000)
-
-let iter_rectangle a b ~f =
-  let ax, ay = quorem a in
-  let bx, by = quorem b in
+let iter_rectangle (ax, ay) (bx, by) ~f =
   for x = ax to bx do
     for y = ay to by do
-      f (key x y)
+      f (x, y)
     done
   done
 
-let interpret s (op, a, b) =
-  match op with
-  | Turn_on -> iter_rectangle a b ~f:(Hash_set.add s)
-  | Turn_off -> iter_rectangle a b ~f:(Hash_set.remove s)
-  | Toggle ->
-      iter_rectangle a b ~f:(fun pos ->
-          if Hash_set.mem s pos then Hash_set.remove s pos
-          else Hash_set.add s pos)
+let array_update t (x, y) ~f =
+  let v = t.(x).(y) in
+  let v' = f v in
+  t.(x).(y) <- v'
+
+let interpret t (op, a, b) =
+  let f =
+    match op with
+    | Turn_on -> fun _ -> true
+    | Turn_off -> fun _ -> false
+    | Toggle -> not
+  in
+  iter_rectangle a b ~f:(array_update t ~f)
+
+let array_fold_matrix t ~f ~init =
+  Array.fold t ~init ~f:(fun acc ar -> Array.fold ar ~init:acc ~f)
 
 let f1 s =
-  let set = Hash_set.create (module Int) in
-  String.split_lines s |> List.map ~f:parse |> List.iter ~f:(interpret set);
-  Hash_set.length set
+  let t = Array.make_matrix ~dimx:1000 ~dimy:1000 false in
+  String.split_lines s |> List.map ~f:parse |> List.iter ~f:(interpret t);
+  array_fold_matrix t ~init:0 ~f:(fun n b -> if b then n + 1 else n)
 
 let interpret2 t (op, a, b) =
-  iter_rectangle a b ~f:(fun pos ->
-      Hashtbl.update t pos ~f:(fun no ->
-          let n = Option.value no ~default:0 in
-          match op with
-          | Turn_on -> n + 1
-          | Turn_off -> Int.max 0 (n - 1)
-          | Toggle -> n + 2))
+  let f =
+    match op with
+    | Turn_on -> fun n -> n + 1
+    | Turn_off -> fun n -> Int.max 0 (n - 1)
+    | Toggle -> fun n -> n + 2
+  in
+  iter_rectangle a b ~f:(array_update t ~f)
 
 let f2 s =
-  let t = Hashtbl.create (module Int) in
+  let t = Array.make_matrix ~dimx:1000 ~dimy:1000 0 in
   String.split_lines s |> List.map ~f:parse |> List.iter ~f:(interpret2 t);
-  Hashtbl.fold t ~init:0 ~f:(fun ~key:_ ~data n -> data + n)
+  array_fold_matrix t ~init:0 ~f:( + )
 
 let run () = Run.run ~name:"day06" ~f1 ~f2 Day06_input.data
