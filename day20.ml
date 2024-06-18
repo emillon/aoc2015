938c8504c0b36eabd74be6440d7c49873c202f84
Etienne Millon <me@emillon.org>
2024-06-18 10:46:49 +0200

    day 20 p2

--- day20.ml
+++ day20.ml
@@ -1,3 +1,5 @@
+open Algo
+
 let find_prime_factor0 n =
   let rec go c =
     if c ** 2 > n then None else if n % c = 0 then Some c else go (c + 1)
@@ -9,29 +11,32 @@ let prime_multiplicity n p =
   go n 0
 
 let find_prime_factor n =
-  match find_prime_factor0 n with
-  | None -> None
-  | Some p ->
-      let m, d = prime_multiplicity n p in
-      Some (p, m, d)
+  let%map.Option p = find_prime_factor0 n in
+  let m, d = prime_multiplicity n p in
+  (p, m, d)
 
-let presents_nocache n f =
+let factors_nocache n f =
   match find_prime_factor n with
-  | Some (p, m, d) -> ((p ** (m + 1)) - 1) / (p - 1) * f d
-  | None when n = 1 -> 1
-  | None -> n + 1
+  | Some (p, m, d) -> (p, m) :: f d
+  | None when n = 1 -> []
+  | None -> [ (n, 1) ]
 
-let rec presents =
+let rec factors =
   let cache = ref (Map.empty (module Int)) in
   fun n ->
     match Map.find !cache n with
     | Some r -> r
     | None ->
-        let r = presents_nocache n presents in
+        let r = factors_nocache n factors in
         cache := Map.add_exn !cache ~key:n ~data:r;
         r
 
-let presents n = 10 * presents n
+let presents n =
+  let s1 =
+    List.fold (factors n) ~init:1 ~f:(fun acc (p, m) ->
+        ((p ** (m + 1)) - 1) / (p - 1) * acc)
+  in
+  10 * s1
 
 let%expect_test "presents" =
   let test n = presents n |> printf "%d" in
@@ -58,13 +63,30 @@ let%expect_test "presents" =
   test 36;
   [%expect {| 910 |}]
 
-let f1 s =
-  let n = String.strip s |> Int.of_string in
-  let rec go i =
-    let r = presents i in
-    if r >= n then i else go (i + 1)
-  in
+let lowest_such_that ~f =
+  let rec go i = if f i then i else go (i + 1) in
   go 1
 
-let f2 _ = 0
+let f1 s =
+  let n = String.strip s |> Int.of_string in
+  lowest_such_that ~f:(fun i -> presents i >= n)
+
+let rec divisors_from_factors f =
+  match f with
+  | [] -> [ 1 ]
+  | (p, m) :: d ->
+      let open List.Let_syntax in
+      let%bind n = divisors_from_factors d in
+      let%map i = List.range 0 m ~stop:`inclusive in
+      (p ** i) * n
+
+let presents2 n =
+  factors n |> divisors_from_factors
+  |> List.filter ~f:(fun d -> n / d <= 50)
+  |> sum |> ( * ) 11
+
+let f2 s =
+  let n = String.strip s |> Int.of_string in
+  lowest_such_that ~f:(fun i -> presents2 i >= n)
+
 let run () = Run.run ~name:"day20" ~f1 ~f2 Day20_input.data
